ab82ad916e2835a2e1e6fc8f4b2317126a84b07d
kotakvishal <kotakvishal2001@gmail.com>
2025-12-30 11:22:35 +0530

    Fix:Make root as lazy column with scroll state instead of lazy column to prevent lag
    Signed-off-by: kotakvishal <kotakvishal2001@gmail.com>

--- app/src/main/java/ktk/wishdroid/grocery_app_compose_ui/ui/screens/HomeScreen.kt
+++ app/src/main/java/ktk/wishdroid/grocery_app_compose_ui/ui/screens/HomeScreen.kt
@@ -1,10 +1,14 @@
 package ktk.wishdroid.grocery_app_compose_ui.ui.screens
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -27,48 +31,33 @@ fun HomeScreen() {
     Scaffold(
         containerColor = Color(0xFF0E0E0E), bottomBar = { BottomNav() }) { padding ->
 
-        LazyColumn(
+        Column (
             modifier = Modifier
                 .padding(padding)
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize()
         ) {
-
-            item { TopLocationBar() }
-            item { SearchBar() }
-            item { CategoryTabRow() }
-            item { PromoBanner() }
-
-            item {
-                SectionTitle(
-                    title = "Top selling fruits", modifier = Modifier.padding(horizontal = 16.dp)
+            TopLocationBar()
+            SearchBar()
+            CategoryTabRow()
+            PromoBanner()
+            SectionTitle(
+                title = "Top selling fruits", modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            ProductRow(
+                products = topSellingProducts,
+                modifier = Modifier.padding(top = 12.dp)
+            )
+            SectionTitle(
+                title = "Explore more", modifier = Modifier.padding(
+                    start = 16.dp, end = 16.dp, top = 24.dp
                 )
-            }
 
-            item {
-                ProductRow(
-                    products = topSellingProducts,
-                    modifier = Modifier.padding(top = 12.dp)
-                )
-            }
-
-
-
-
-            item {
-                SectionTitle(
-                    title = "Explore more", modifier = Modifier.padding(
-                        start = 16.dp, end = 16.dp, top = 24.dp
-                    )
-
-                )
-            }
-            item {
-                ProductGrid(
-                    products = exploreMoreProducts,
-                    modifier = Modifier.padding(top = 12.dp)
-                )
-            }
-
+            )
+            ProductGrid(
+                products = exploreMoreProducts,
+                modifier = Modifier.padding(top = 12.dp)
+            )
         }
     }
 }
